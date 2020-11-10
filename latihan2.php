9cd36b2721df11a16adeae8818aad591cebd4a38
nagachi <rafly.rigannagachi@yahoo.com>
2020-11-10 11:41:50 +0700

    initial commit

---
+++ latihan2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+    <?php
+        $nama = [
+            "lanirne", "fulan", "alexander",
+            "napoleon", "kifuat", "samidi",
+            "paijo", "dukindam", "aben", "udi"
+        ];
+        foreach($values as $nama){
+            for(int $i=0; $i<$nama)
+            $values;
+        }
+    ?>    
+</body>
+</html>
