8ab28c13e40b69465f8e6f732d3dac48088af931
nagachi <rafly.rigannagachi@yahoo.com>
2020-11-17 21:33:14 +0700

    fix faktorial only for positive number

--- PHP Lanjut/tugas1.php
+++ PHP Lanjut/tugas1.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <?php
     function faktorial($bil){
-        if($bil==1){
+        if($bil==0){
             return 1;
         }else{
             return $bil*faktorial($bil-1);
@@ -11,7 +11,7 @@
 <h2>Faktorial</h2>
 <form method="post">
     <label>Bilangan: </label>
-    <input type="number" name="bilangan">
+    <input type="number" name="bilangan" min="0">
     <button type="submit" name="kirim">submit</button>
 </form><br>
 <?php
