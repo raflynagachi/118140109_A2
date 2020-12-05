e21ad7b2a8b4953a73a52ab943e1c403841c9785
nagachi <rafly.rigannagachi@yahoo.com>
2020-12-05 08:31:12 +0700

    add tugas 2: fungsi dom and fix some dom in tugas 1

---
+++ tugas-javascript/fungsiDom.js
@@ -0,0 +1,5 @@
+console.log(document.getElementById("1"));
+console.log(document.getElementsByTagName("button"));
+console.log(document.getElementsByClassName("jumbotron"));
+console.log(document.querySelector("li"));
+console.log(document.querySelectorAll("li"));
